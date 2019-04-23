c20570e10f4efa8f35c2c65d580feec908434339
adewalesamuel <37175266+adewalesamuel@users.noreply.github.com>
2019-04-23 20:57:29 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const conn = mongoose.connect( mongoUri, {useNewUrlParser: true} )
 const PORT = process.env.PORT || 3000
 
 const userRoutes = require('./routes/user')
+const relationRoutes = require('./routes/relation')
 
 app.use(bodyParser.json())
 
 app.use('/user', userRoutes)
+app.use('/relation', relationRoutes)
+
 app.listen(3000)
